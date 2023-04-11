1451b52e20e3d5f5bb57312c451bd337ff182bcf
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-04-11 18:59:26 -0400

    multithreading with goroutine

--- main.go
+++ main.go
@@ -2,44 +2,54 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"sync"
+	"time"
 )
 
 const conferenceTickets int = 50
 
 var conferenceName = "Go Conference"
 var remainingTickets uint = 50
-var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets int
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	greetUsers()
 
-	for {
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		if isValidName && isValidEmail && isValidTicketNumber {
-			bookTicket(uint(userTickets), firstName, lastName, email)
+	if isValidName && isValidEmail && isValidTicketNumber {
+		bookTicket(uint(userTickets), firstName, lastName, email)
+		wg.Add(1)
+		go sendTicket(uint(userTickets), firstName, lastName, email)
 
-			firstNames := getFirstNames()
-			fmt.Printf("The following people have booked tickets: %v.\n", firstNames)
+		firstNames := getFirstNames()
+		fmt.Printf("The following people have booked tickets: %v.\n", firstNames)
 
-			if remainingTickets == 0 {
-				fmt.Println("Sorry, we are out of tickets.")
-				break
-			}
-		} else {
-			if !isValidName {
-				fmt.Println("First name and last name must be at least 2 characters long.")
-			}
-			if !isValidEmail {
-				fmt.Println("Email must contain @ and .")
-			}
-			if !isValidTicketNumber {
-				fmt.Printf("Number of tickets must be between 1 and %v.\n", remainingTickets)
-			}
+		if remainingTickets == 0 {
+			fmt.Println("Sorry, we are out of tickets.")
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("First name and last name must be at least 2 characters long.")
+		}
+		if !isValidEmail {
+			fmt.Println("Email must contain @ and .")
+		}
+		if !isValidTicketNumber {
+			fmt.Printf("Number of tickets must be between 1 and %v.\n", remainingTickets)
 		}
 	}
+	wg.Wait()
 }
 
 func greetUsers() {
@@ -51,7 +61,7 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -76,15 +86,25 @@ func getUserInput() (string, string, string, int) {
 func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets -= uint(userTickets)
 
-	var userData = make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: int(userTickets),
+	}
 
 	bookings = append(bookings, userData)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. We will send you a confirmation email shortly.\n",
 		firstName, lastName, userTickets)
 	fmt.Printf("We have %v tickets remaining for %v.\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(5 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("###################################################")
+	fmt.Printf("Sending ticket:\n %v \nto email address: %v.\n", ticket, email)
+	fmt.Println("###################################################")
+	wg.Done()
 }
