f0dbb93d7eb45c26d5852c7049ac5d41538564cc
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-04-11 13:32:09 -0400

    prompt user inputs

--- main.go
+++ main.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	conferenceTickets = 50
-	fmt.Println("Welcome to", conferenceName, "booking application.")
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	fmt.Printf("type of conferenceName: %T\n", conferenceName)
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask the user for their name
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
+	fmt.Println("Please enter the number of tickets you want to book:")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. We will send you a confirmation email shortly.\n", firstName, lastName, userTickets)
 }
