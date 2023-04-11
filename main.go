1fd0af51e8799a9cb4ced9484de6a89ede2e44d8
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-04-11 13:51:44 -0400

    store user info

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	fmt.Printf("type of conferenceName: %T\n", conferenceName)
+	var bookings []string
+
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
@@ -15,7 +16,7 @@ func main() {
 	var lastName string
 	var email string
 	var userTickets int
-	// ask the user for their name
+
 	fmt.Println("Please enter your first name:")
 	fmt.Scan(&firstName)
 	fmt.Println("Please enter your last name:")
@@ -24,5 +25,11 @@ func main() {
 	fmt.Scan(&email)
 	fmt.Println("Please enter the number of tickets you want to book:")
 	fmt.Scan(&userTickets)
+
+	remainingTickets -= uint(userTickets)
+	bookings = append(bookings, firstName+" "+lastName)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. We will send you a confirmation email shortly.\n", firstName, lastName, userTickets)
+	fmt.Printf("We have %v tickets remaining for %v.\n", remainingTickets, conferenceName)
+	fmt.Printf("The following people have booked tickets: %v.\n", bookings)
 }
